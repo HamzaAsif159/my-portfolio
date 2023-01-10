c7d7621669e63ae626aaffbee89fab13fb4ded80
HamzaAsif159 <hmziiiraj@gmail.com>
2023-01-10 17:26:24 +0500

    Added Favorite projects

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Navbar from "../components/Navbar"
 import Container from "../components/Container"
 import Hero from "../components/Hero"
+import FavoriteProjects from "../components/FavoriteProjects"
+
 export default function Home() {
   return (
     <>
       <Container>
         <Hero />
+        <FavoriteProjects />
       </Container>
     </>
   )
