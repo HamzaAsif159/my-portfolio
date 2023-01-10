fd8a51f43c229ccc7f8c25047e1b9e9cbb3661bf
HamzaAsif159 <hmziiiraj@gmail.com>
2023-01-10 17:08:01 +0500

    initial files

---
+++ components/Container.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import Navbar from "./Navbar"
+import Footer from "./footer"
+
+export default function Container({ children, ...customMeta }) {
+  const router = useRouter()
+
+  const meta = {
+    title: "Hamza Asif - React Developer",
+    description: ` Get in touch with me to know more.`,
+    image: "https://i.ibb.co/7VNwPRF/fav.jpg",
+    type: "website",
+    ...customMeta,
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://yourwebsite.com${router.asPath}`}
+        />
+        <link
+          rel="canonical"
+          href={`https://yourwebsite.com${router.asPath}`}
+        />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Hamza Asif" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {/* Twitter meta card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@hamzicodes" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
+      <main className="bg-gray-800 w-full">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </main>
+    </div>
+  )
+}
---
+++ constant/Data.js
@@ -0,0 +1,103 @@
+const Data = {
+  githubUsername: "HamzaAsif159",
+  name: "Hamza Asif",
+  designation: "Jr. React Developer",
+  avatarUrl: "https://i.ibb.co/7VNwPRF/fav.jpg",
+  email: "hmziiiraj@gmail.com",
+  phone: "+92 3249962290",
+  address: "Faisalabad, Punjab, Pakistan",
+  projects: [
+    {
+      title: "Weatherbug",
+      link: "https://react-weather-app233.netlify.app",
+      imgUrl:
+        "https://i.ibb.co/5R7VHRY/Screenshot-from-2023-01-04-18-02-35.png",
+    },
+    {
+      title: "Pictobook",
+      link: "https://pictobook.netlify.app/",
+      imgUrl:
+        "https://i.ibb.co/F8F20cH/Screenshot-from-2023-01-04-18-05-46.png",
+    },
+    {
+      title: "Quizzical",
+      link: "https://quizzical159.netlify.app/",
+      imgUrl:
+        "https://i.ibb.co/jbmS8PV/Screenshot-from-2023-01-04-18-11-46.png",
+    },
+    {
+      title: "Library App",
+      link: "https://library-app-1.netlify.app/",
+      imgUrl:
+        "https://i.ibb.co/sWD8rrj/Screenshot-from-2023-01-04-18-20-46.png",
+    },
+    {
+      title: "Tic Tac Toe",
+      link: "https://tic-tac-11.netlify.app/",
+      imgUrl:
+        "https://i.ibb.co/tCb74KC/Screenshot-from-2023-01-04-18-35-25.png",
+    },
+    {
+      title: "meme Generator",
+      link: "https://meme-generator-2222.netlify.app",
+      imgUrl:
+        "https://i.ibb.co/HKzx1ky/Screenshot-from-2023-01-04-18-41-53.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a React developer that loves building products and web applications that are helpful for daily use",
+    description: [
+      ` I'm a React Developer with hands-on experience in identifying web-based user interactions along with designing and implementing highly-responsive user interface components by deploying React concepts. Proficient in translating designs and wire-frames into high quality code, and writing applications interface code via JavaScript and ReactJS workflows. Adept at monitoring and maintaining front-end performance, troubleshooting and debugging the same to bolster overall performance.`,
+    ],
+    currentProject: "Redux E-commerce Store",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Intern React Developer",
+      company: "Strategist Hub",
+      year: "2022-Present",
+      companyLink: "https://strategisthub.com",
+      desc: "Working as a React Intern for developing sweet and reusbale React components",
+    },
+    {
+      title: "Social Media Marketer",
+      company: "Strategist Hub",
+      year: "2020-2022",
+      companyLink: "https://strategisthub.com/",
+      desc: "Worked for company social apps and posting",
+    },
+    {
+      title: "Graduation",
+      company: "The University Of Faisalabad",
+      year: "2020-2024",
+      companyLink: "https://tuf.edu.pk/",
+      desc: "Bachelor's degree of Computer Science with 3.4 CGPA but that's okay nobody asks for.",
+    },
+    {
+      title: "Intermediate",
+      company: "Punjab College, Pindi Bhattian, Punjab, Pakistan.",
+      year: "2020",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science with 80% marks",
+    },
+    {
+      title: "High School",
+      company: "District Public School, Pindi Bhattian, Punjab, Pakistan.",
+      year: "2018",
+      companyLink: "https://upes.co.in",
+      desc: "Did matriculation and barely passed with 85% marks",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    instagram: "https://www.instagram.com/_hamziii__",
+    twitter: "https://twitter.com/hamzicodes",
+    linkedin: "https://www.linkedin.com/in/hamzii/",
+    github: "https://github.com/HamzaAsif159",
+    facebook: "https://www.facebook.com/hamza.rajput.35728466?mibextid=ZbWKwL",
+  },
+}
+
+export default Data
